914e0b81f2c06e9964bf37b0b36e925a14554176
Ajay Kushwaha <ajkushwaha1112@gmail.com>
2024-03-19 23:43:44 +0530

    header functionality

--- components/about.tsx
+++ components/about.tsx
@@ -1,13 +1,28 @@
 "use client";
 
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useEffect } from "react";
 import SectionHeading from "./section-heading";
+import { useInView } from "react-intersection-observer";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function About() {
+  const { ref, inView } = useInView({
+    threshold: 0.75,
+  });
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("About");
+    }
+  }, [inView, setActiveSection]);
+
   return (
     <motion.section
-      className="max-w-[45rem] text-center leading-8"
+      ref={ref}
+      id="about"
+      className="max-w-[45rem] text-center leading-8 scroll-mt-24"
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
--- components/header.tsx
+++ components/header.tsx
@@ -4,8 +4,10 @@ import React from "react";
 import { motion } from "framer-motion";
 import { links } from "@/lib/data";
 import Link from "next/link";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Header() {
+  const { activeSection, setActiveSection } = useActiveSectionContext();
   return (
     <header className="z-[999] relative">
       <motion.div
@@ -15,7 +17,7 @@ export default function Header() {
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       ></motion.div>
-      <nav className="flex fixed left-1/2 h-12 -translate-x-1/2 py-2 top-0 sm:top-[1.6rem] sm:h-[initial] sm:py-0">
+      <nav className="flex fixed left-1/2 h-12 -translate-x-1/2 py-3 top-0 sm:top-[1.8rem] sm:h-[initial] sm:py-0">
         <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
           {links.map((link) => (
             <motion.li
@@ -25,10 +27,24 @@ export default function Header() {
               animate={{ y: 0, opacity: 1 }}
             >
               <Link
-                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition"
+                className={`flex w-full items-center justify-center px-3 py-2 hover:text-gray-950 transition ${
+                  activeSection === link.name ? "text-gray-950" : ""
+                }`}
                 href={link.hash}
+                onClick={() => setActiveSection(link.name)}
               >
                 {link.name}
+                {activeSection === link.name && (
+                  <motion.span
+                    className="bg-gray-100 rounded-full absolute inset-0 -z-10"
+                    layoutId="activeSection"
+                    transition={{
+                      type: "spring",
+                      stiffness: 380,
+                      damping: 30,
+                    }}
+                  ></motion.span>
+                )}
               </Link>
             </motion.li>
           ))}
